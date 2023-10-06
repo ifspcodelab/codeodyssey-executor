4af8c5deb25b470811c6f796b1a9885c292ae8a8
rafaelspa <rsdepaula67@gmail.com>
2023-10-06 15:32:42 -0300

    refactor: move containerizer

--- Containerizer/containerizer.py
+++ Containerizer/containerizer.py
@@ -23,7 +23,7 @@ with open('.Dockerfile', 'w') as file:
 
 # build an image from the Dockerfile
 temurin_gradlew_image, build_logs = docker_client.images.build(
-    path=r'.',
+    path=r'..',
     dockerfile='.Dockerfile',
     tag=image_tag_name,
     rm=True
