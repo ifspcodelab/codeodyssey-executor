4862fd5682fc157e94ae6a31e1e041fbbb754eee
Sergio <sergioadm120@gmail.com>
2023-10-11 12:30:54 -0300

    feat: retrieve resolutionFile and testFile from the database tables

--- Executor.py
+++ Executor.py
@@ -10,12 +10,43 @@ connection = psycopg2.connect(
     host="localhost",
     database="postgres",
     user="postgres",
-    password="postgres"
+    password="postgres",
+    port="5432"
 )
+
+cursor = connection.cursor()
+
 
 def callback(ch, method, properties, body):
     print(f"Iniciando processamento: {body}")
+
+    id_resolution = body
+
+    cursor.execute("""
+        SELECT 
+            resolutionFile, testFile, extension
+        FROM
+            RESOLUTION, ACTIVITY
+        WHERE
+            RESOLUTION.idActivity = ACTIVITY.id AND RESOLUTION.id = %s 
+    """, (id_resolution,))
+
+    result = cursor.fetchone()
+
+    resolution_file_bytes = bytes.fromhex(result[0][2:])
+    test_file_bytes = bytes.fromhex(result[1][2:])
+
+    decoded_resolution_file_str = base64.b64decode(resolution_file_bytes.decode('utf-8')).decode('utf-8')
+    decoded_test_file_str = base64.b64decode(test_file_bytes.decode('utf-8')).decode('utf-8')
+
+    with open(f'resolutionFile.{result[2]}', 'w') as f:
+        f.write(decoded_resolution_file_str)
+
+    with open(f'testFile.{result[2]}', 'w') as f:
+        f.write(decoded_test_file_str)
+
     run_containerizer()
+
     print(f"Concluído processamento: {body}")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
